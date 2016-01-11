91836f487b36ccb2d6ff816537db3a0b6931507f
Barbonz <barbonz@hotmail.it>
2016-01-11 23:19:47 +0100

    Menu
    Ho fatto il main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+    public Canvas MainCanavas;
+ 
+    public void ReturnOn()
+    {
+        MainCanavas.enabled = true;
+    }
+
+    public void LoadOn()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void OnGUI()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
