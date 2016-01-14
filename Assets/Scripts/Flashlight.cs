8abbacf379fc240e4f0af635aa8e8a0aa8704b22
Max <massimiliano.ariani@gmail.com>
2016-01-14 20:00:17 +0100

    Trovato un piccolo bug sui nomi di 2 script Corretto

--- Assets/Scripts/Flashlight.cs
+++ Assets/Scripts/Flashlight.cs
@@ -1,41 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
-//Script per la gestione dell'illuminazione del gioco
-public class LuceDown : MonoBehaviour {
-	public GameObject stonePrefab;
-	public Transform Spawn;
+//ATTIVAZIONE E DISATTIVAZIONE DELLA LAMPADA DEL PG
+public class Flashlight : MonoBehaviour
+{
+    Light flashlight;
+    public bool on = false;
+    // Use this for initialization
+    void Start()
+    {
+        flashlight = GetComponentInChildren<Light>();
+    }
 
-
-	// Use this for initialization
-	void Start () 
-	{
-	
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-
-
-			if (Input.GetKeyDown(KeyCode.E))
-			{
-			stonePrefab = Instantiate(stonePrefab, Spawn.transform.position, Quaternion.identity) as GameObject;
-
-			}
-		}
-		/*if (Input.GetKey(KeyCode.E))
-		{
-		
-			//public GameObject EnemyPrefab;
-			//public Transform[] spawnPoint;
-			//EnemyPrefab = Instantiate(EnemyPrefab, spawnPoint[range].position, EnemyPrefab.transform.rotation) as GameObject;
-
-			stonePrefab = Instantiate(stonePrefab, Spawn.transform.position, stonePrefab.transform.rotation) as GameObject;
-			
-		}*/
-	
-	}
-
-
-
-
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+            on = !on;
+        if (on)
+            flashlight.enabled = true;
+        else if (!on)
+            flashlight.enabled = false;
+    }
+}
---
+++ Assets/Scripts/LuceDown.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+//Script per la creazione delle sfere luminose dal player
+public class LuceDown : MonoBehaviour {
+	public GameObject stonePrefab;
+	public Transform Spawn;
+
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+			if (Input.GetKeyDown(KeyCode.E))
+			{
+			    stonePrefab = Instantiate(stonePrefab, Spawn.transform.position, Quaternion.identity) as GameObject;
+			}
+		}
+		/* Codice non utilizzato ma possibilita' di riutilizzo
+        if (Input.GetKey(KeyCode.E))
+		{
+		
+			//public GameObject EnemyPrefab;
+			//public Transform[] spawnPoint;
+			//EnemyPrefab = Instantiate(EnemyPrefab, spawnPoint[range].position, EnemyPrefab.transform.rotation) as GameObject;
+
+			stonePrefab = Instantiate(stonePrefab, Spawn.transform.position, stonePrefab.transform.rotation) as GameObject;
+			
+		}*/
+	
+	}
+
+
+
+
