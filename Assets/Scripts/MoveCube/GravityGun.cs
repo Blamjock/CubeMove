9032b8751a87155a466a234b4d8c91a93caee237
Max <massimiliano.ariani@gmail.com>
2016-01-15 15:54:21 +0100

    Upload file mancanti per gestire il player

---
+++ Assets/Scripts/MoveCube/GravityGun.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityGun : MonoBehaviour {
+
+    public float rootSpeed = 0.0f;
+    private Transform target;
+    RaycastHit hit;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey(KeyCode.A))
+        {
+            transform.RotateAround(transform.position, Vector3.up, -rotSpeed * Time.deltaTime);
+
+        }
+        if {Input.GetKey (Key)
+                
+        }
+	}
+}
