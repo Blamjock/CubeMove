35ce0558b0ce02c113609834be27ad14f025f84e
Max <massimiliano.ariani@gmail.com>
2016-01-15 16:05:51 +0100

    Inserito Script Gravity Gun

--- Assets/Scripts/MoveCube/GravityGun.cs
+++ Assets/Scripts/MoveCube/GravityGun.cs
@@ -17,11 +17,33 @@ public class GravityGun : MonoBehaviour {
 	void Update () {
 	    if (Input.GetKey(KeyCode.A))
         {
-            transform.RotateAround(transform.position, Vector3.up, -rotSpeed * Time.deltaTime);
+            transform.RotateAround(transform.position, Vector3.up, -rootSpeed * Time.deltaTime);
 
         }
-        if {Input.GetKey (Key)
-                
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.RotateAround(transform.position, Vector3.up, rootSpeed * Time.deltaTime);
+
         }
-	}
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                Debug.Log("you select the " + hit.transform.name);
+                hit.transform.parent = transform;
+                hit.transform.GetComponent<Rigidbody>().useGravity = false;
+                float hitPosY = hit.transform.position.y + 2.0f;
+                hit.transform.position = new Vector3(hit.transform.position.x, hitPosY, hit.transform.position.z);
+
+            }
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            hit.transform.parent = null;
+            hit.transform.GetComponent<Rigidbody>().useGravity = true;
+
+        }
+    }
 }
