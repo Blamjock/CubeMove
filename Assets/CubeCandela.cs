e4b12c793c20252437c4bc05c1d2ca608e17e482
ghirlo85 <ghirlo85@gmail.com>
2016-01-20 18:47:28 +0100

    Test light
    test per vedere come sistemare la scarica della batteria

---
+++ Assets/CubeCandela.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeCandela : MonoBehaviour {
+	Light flashlight;
+	public float number=1.0f;
+	public float EnergyLuce = 0.0f;
+	// Use this for initialization
+	void Start()
+	{
+		flashlight = GetComponentInChildren<Light>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		EnergyLuce += number + Time.deltaTime;
+
+		if (EnergyLuce >= 1000.0f) {
+
+			flashlight.enabled = false;
+		}
+	}
+}
